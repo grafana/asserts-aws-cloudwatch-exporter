00cb8f333fd9a28a94ceb82579aad3787a919e41
jradhakrishnan <radhakrishnan.jankiraman@gmail.com>
2021-11-26 16:56:48 +0530

    Increase scrape interval to keep costs under control

--- src/main/java/ai/asserts/aws/MetadataTaskManager.java
+++ src/main/java/ai/asserts/aws/MetadataTaskManager.java
@@ -58,8 +58,8 @@ public class MetadataTaskManager implements InitializingBean {
     }
 
     @SuppressWarnings("unused")
-    @Scheduled(fixedRateString = "${aws.metric.scrape.manager.task.fixedDelay:300000}",
-            initialDelayString = "${aws.metric.scrape.manager.task.initialDelay:5000}")
+    @Scheduled(fixedRateString = "${aws.metadata.scrape.manager.task.fixedDelay:300000}",
+            initialDelayString = "${aws.metadata.scrape.manager.task.initialDelay:5000}")
     @Timed(description = "Time spent scraping AWS Resource meta data from all regions", histogram = true)
     public void updateMetadata() {
         taskThreadPool.getExecutorService().submit(lambdaCapacityExporter::update);
