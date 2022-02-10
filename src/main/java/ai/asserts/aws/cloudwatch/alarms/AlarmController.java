cc0b2f99461d144bd0f9b1402a95d14aef3cbd97
Shanavazh <shanavazh@gmail.com>
2022-02-10 10:59:47 +0530

    alarm request object mapping

--- src/main/java/ai/asserts/aws/cloudwatch/alarms/AlarmController.java
+++ src/main/java/ai/asserts/aws/cloudwatch/alarms/AlarmController.java
@@ -4,6 +4,8 @@
  */
 package ai.asserts.aws.cloudwatch.alarms;
 
+import ai.asserts.aws.ObjectMapperFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 
@@ -25,23 +28,15 @@ public class AlarmController {
     private static final String ALARMS = "/receive-cloudwatch-alarms";
     private final AlarmMetricConverter alarmMetricConverter;
     private final AlarmMetricExporter alarmMetricExporter;
+    private final ObjectMapperFactory objectMapperFactory;
 
     @PostMapping(
             path = ALARMS,
             produces = APPLICATION_JSON_VALUE,
             consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<AlarmResponse> receiveAlarmsPost(
-            @RequestBody AlarmStateChange alarmStateChange) {
-        try {
-            List<Map<String, String>> alarmsLabels = this.alarmMetricConverter.convertAlarm(alarmStateChange);
-            if (!CollectionUtils.isEmpty(alarmsLabels)) {
-                alarmMetricExporter.processMetric(alarmsLabels);
-                return ResponseEntity.ok(AlarmResponse.builder().status("Success").build());
-            }
-        } catch (Exception ex) {
-            log.error("Error in processing {}-{}", ex.toString(), ex.getStackTrace());
-        }
-        return ResponseEntity.unprocessableEntity().build();
+            @RequestBody AlarmRequest alarmRequest) {
+        return processRequest(alarmRequest);
     }
 
     @PutMapping(
@@ -49,16 +44,35 @@ public class AlarmController {
             produces = APPLICATION_JSON_VALUE,
             consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<AlarmResponse> receiveAlarmsPut(
-            @RequestBody AlarmStateChange alarmStateChange) {
+            @RequestBody AlarmRequest alarmRequest) {
+        return processRequest(alarmRequest);
+    }
+
+    private ResponseEntity<AlarmResponse> processRequest(AlarmRequest alarmRequest) {
         try {
-            List<Map<String, String>> alarmsLabels = this.alarmMetricConverter.convertAlarm(alarmStateChange);
-            if (!CollectionUtils.isEmpty(alarmsLabels)) {
-                alarmMetricExporter.processMetric(alarmsLabels);
-                return ResponseEntity.ok(AlarmResponse.builder().status("Success").build());
+            boolean status = true;
+            if (!CollectionUtils.isEmpty(alarmRequest.getRecords())) {
+                for (AlarmRecord alarmRecord : alarmRequest.getRecords()) {
+                    status = status && accept(alarmRecord);
+                }
+                if (status) {
+                    return ResponseEntity.ok(AlarmResponse.builder().status("Success").build());
+                }
             }
         } catch (Exception ex) {
             log.error("Error in processing {}-{}", ex.toString(), ex.getStackTrace());
         }
         return ResponseEntity.unprocessableEntity().build();
+    }
+
+    private boolean accept(AlarmRecord data) throws JsonProcessingException {
+        String alarmStateChange = new String(Base64.getDecoder().decode(data.getData()));
+        AlarmStateChange alarmStateChange1 = objectMapperFactory.getObjectMapper().readValue(alarmStateChange, AlarmStateChange.class);
+        List<Map<String, String>> alarmsLabels = this.alarmMetricConverter.convertAlarm(alarmStateChange1);
+        if (!CollectionUtils.isEmpty(alarmsLabels)) {
+            alarmMetricExporter.processMetric(alarmsLabels);
+            return true;
+        }
+        return false;
     }
 }
---
+++ src/main/java/ai/asserts/aws/cloudwatch/alarms/AlarmRequest.java
@@ -0,0 +1,28 @@
+/*
+ *  Copyright © 2021.
+ *  Asserts, Inc. - All Rights Reserved
+ */
+package ai.asserts.aws.cloudwatch.alarms;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@EqualsAndHashCode
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AlarmRequest {
+    private String requestId;
+    private String timestamp;
+    private List<AlarmRecord> records;
+}
