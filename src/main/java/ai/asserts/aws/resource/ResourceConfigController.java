773f874b1c4872be99a5a3f312f60c666ebe34eb
Shanavazh <shanavazh@gmail.com>
2022-03-09 11:23:33 +0530

    resource config change support

---
+++ src/main/java/ai/asserts/aws/resource/ResourceConfigController.java
@@ -0,0 +1,31 @@
+/*
+ *  Copyright © 2021.
+ *  Asserts, Inc. - All Rights Reserved
+ */
+package ai.asserts.aws.resource;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+public class ResourceConfigController {
+    private static final String CONFIG_EVENTS ="/receive-config-events";
+
+    @PostMapping(
+            path = CONFIG_EVENTS,
+            produces = APPLICATION_JSON_VALUE,
+            consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> receiveConfigEvents(
+            @RequestBody Object configEvents){
+        log.info(configEvents.toString());
+        return ResponseEntity.ok("Completed");
+    }
+}
