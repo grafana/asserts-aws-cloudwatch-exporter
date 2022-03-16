bbf652b6da33133bb36da33d39d8d64602473d7b
Shanavazh <shanavazh@gmail.com>
2022-03-16 14:42:30 +0530

    Adding job label for KinesisAnalytics

--- src/main/java/ai/asserts/aws/exporter/KinesisAnalyticsExporter.java
+++ src/main/java/ai/asserts/aws/exporter/KinesisAnalyticsExporter.java
@@ -81,6 +81,9 @@ public class KinesisAnalyticsExporter extends Collector implements InitializingB
                                 resource.addLabels(labels, "");
                                 labels.put("aws_resource_type", labels.get("type"));
                                 labels.remove("type");
+                                if (labels.containsKey("name")) {
+                                    labels.put("job", labels.get("name"));
+                                }
                                 return sampleBuilder.buildSingleSample("aws_resource", labels, 1.0D);
                             })
                             .collect(Collectors.toList()));
